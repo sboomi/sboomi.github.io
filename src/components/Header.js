5e6c3ad4ac259dcbfe26ffc4212cb4c598084268
Shadi Boomi <shadi.boomi@gmail.com>
2021-05-18 09:32:53 +0200

    added title

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="App-header">
+      <h1>Shadi Boomi</h1>
+      <h3>My personal page</h3>
+    </div>
+  );
+};
+
+export default Header;
