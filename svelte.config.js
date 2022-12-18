cf50c2f90b12bc3764365285edff4d75518d924f
Shadi Boomi <shadi.boomi@gmail.com>
2022-12-18 23:42:00 +0100

    added tailwind to project

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,8 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-gh-pages' : ''
+			// base: process.env.NODE_ENV === 'production' ? '/sveltekit-gh-pages' : ''
+			base: ''
 		}
 	}
 };
