f012e72ba78efe28c2f3d1913c663534d93995b0
Shadi Boomi <shadi.boomi@gmail.com>
2025-03-31 18:52:37 +0200

    dependency bump and added copy to clipboard

---
+++ src/components/MarkdownFormatter.tsx
@@ -0,0 +1,11 @@
+import Markdown from "react-markdown";
+
+export function MarkdownFormatter({ content }: { content: string }) {
+  return (
+    <div className="prose">
+      <Markdown>{content}</Markdown>
+    </div>
+  );
+}
+
+export default MarkdownFormatter;
---
+++ src/lib/formatter.ts
@@ -0,0 +1,39 @@
+import {  Resume } from "../types";
+
+export function formatResumeToMarkdown(resumeObject: Resume, lang: "en" | "fr"): string {
+    const { skills, languages, experiences, projects, education } = resumeObject;
+
+    const skillsList = skills.map((skill) => `- **${skill.name}**: ${skill.description}`).join("\n");
+
+    const experiencesList = experiences?.map((exp) => `
+## ${exp.title} - ${exp.company} - ${exp.location}
+### ${exp.startDate} - ${exp.endDate}
+${exp.tasks.map(task => `- ${task}`).join("\n")}`).join("\n") || (lang === "en" ? "No experiences available" : "Sans expérience professionelle");
+
+    const projectsList = projects?.map((proj) => `
+## ${proj.title}
+${proj.tasks}`).join("\n") || lang === "en" ? "No personal project" : "Pas de projet personnel";
+
+    const educationList = education.map((edu) => `- ${edu.school}, ${edu.degree}, ${edu.gradYear}`).join("\n");
+
+    const languagesList = languages ? languages.map(lang => `- ${lang.name}: ${lang.level}`).join("\n") : (lang === "en" ? "No languages other than English" : "Pas d'autre langage")
+
+    return `# Shadi BOOMI
+
+# ${lang === "en" ? "Skills" : "Compétences"}
+${skillsList}
+
+## ${lang === "en" ? "Languages" : "Langues"}
+${languagesList}
+
+# ${lang === "en" ? "Experiences" : "Expérience professionnelle"}
+${experiencesList}
+
+# ${lang === "en" ? "Projects" : "Projets"}
+${projectsList}
+
+# ${lang === "en" ? "Education" : "Formation"}
+${educationList}
+`;
+};
+
