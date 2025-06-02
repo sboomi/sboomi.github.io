8e6bf93fc0ef8f21d855590324ac7f5693497418
Shadi Boomi <shadi.boomi@gmail.com>
2025-06-02 18:31:00 +0200

    Hotfix on types

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,24 @@
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Navbar } from "./Navbar";
+import React from "react";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<div className="min-h-screen flex flex-col">
-			<Header />
-			<div className="flex flex-1 pt-16">
-				{/* Sidebar container */}
-				<div className="w-64 bg-gray-100 fixed top-16 bottom-0 left-0 flex flex-col">
-					<Navbar />
-					<div className="flex-1" />{" "}
-					{/* This empty div will push the footer to the bottom */}
-					<Footer />
-				</div>
-				<main className="ml-64 flex-1 p-6 overflow-y-auto">{children}</main>
-			</div>
-		</div>
-	);
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1 pt-16">
+        {/* Sidebar container */}
+        <div className="w-64 bg-gray-100 fixed top-16 bottom-0 left-0 flex flex-col">
+          <Navbar />
+          <div className="flex-1" />{" "}
+          {/* This empty div will push the footer to the bottom */}
+          <Footer />
+        </div>
+        <main className="ml-64 flex-1 p-6 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
